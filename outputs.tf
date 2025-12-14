87923323b4492cf434e2bdc32acdc3cc5b2c339b
michelledayana <michurivadeneira98@gmail.com>
2025-12-14 17:09:00 -0500

    Add outputs Terraform configuration file

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,12 @@ output "frontend_url" {
   value       = "http://${aws_lb.frontend_alb.dns_name}"
 }
 
+output "frontend_alb_dns" {
+  description = "DNS name of the Frontend ALB"
+  value       = aws_lb.frontend_alb.dns_name
+}
+
 output "frontend_asg_name" {
-  description = "Auto Scaling Group name"
+  description = "Frontend Auto Scaling Group name"
   value       = aws_autoscaling_group.frontend_asg.name
 }
